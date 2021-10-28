f91e18e74d7e41c165dd36cd4ac808aea3736c49
duayder <74006139+duayder@users.noreply.github.com>
2021-10-28 21:28:58 +0400

    Add files via upload

--- HW7/src/com/yaroslav/Bird.java
+++ HW7/src/com/yaroslav/Bird.java
@@ -1,7 +1,23 @@
 package com.yaroslav;
 
 public abstract class Bird {
-    private String feathers;
-    private int layEggs;
+    protected String feathers;
+    protected int layEggs;
     public abstract void Fly();
+
+    public String getFeathers() {
+        return feathers;
+    }
+
+    public void setFeathers(String feathers){
+        this.feathers = feathers;
+    }
+
+    public int getLayEggs(){
+        return layEggs;
+    }
+
+    public void setLayEggs(int layEggs){
+        this.layEggs = layEggs;
+    }
 }
